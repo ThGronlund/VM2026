1841ce2cb9150db8319527b10e876c0cc6c8ea23
ThGronlund <th.gronlund@gmail.com>
2026-06-04 17:58:02 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE = 'vm2026-v1';
+
+self.addEventListener('install', e => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.mode === 'navigate') {
+    e.respondWith(
+      fetch(e.request).then(response => {
+        const clone = response.clone();
+        caches.open(CACHE).then(cache => cache.put(e.request, clone));
+        return response;
+      }).catch(() => caches.match(e.request))
+    );
+  }
+});
